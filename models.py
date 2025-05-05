b09217b073b25e2c0046ed2fee5d4b482fe5c6ef
surikadt <gnidanol@gmail.com>
2025-05-05 16:57:25 +0300

    add classes for custom models

--- models.py
+++ models.py
@@ -4,6 +4,65 @@ import torch.nn as nn
 from sklearn.preprocessing import LabelEncoder
 
 
+class GRUModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.gru = nn.GRU(
+            input_size, hidden_size, num_layers=num_layers, dropout=0.2,
+            batch_first=True
+        )
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, hidden):
+        """
+        Args:
+            x (torch.Tensor): Input (batch_size, seq_len, input_size).
+            hidden (torch.Tensor): Hidden state (num_layers, batch_size, hidden_size).
+
+        Returns:
+            torch.Tensor: Output (batch_size, seq_len, output_size).
+            torch.Tensor: Final hidden state (num_layers, batch_size, hidden_size).
+        """
+
+        out, hidden = self.gru(x, hidden)
+        out = self.fc(out)
+        return out, hidden
+
+    def init_hidden(self, batch_size):
+        return torch.zeros(self.num_layers, batch_size, self.hidden_size)
+
+
+class Autoencoder(nn.Module):
+    def __init__(self, input_dim, latent_dim):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, latent_dim)
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, input_dim)
+        )
+
+    def forward(self, x):
+        """
+        Args:
+            x (torch.Tensor): The input tensor.
+
+        Returns:
+            torch.Tensor: The reconstructed output tensor.
+            torch.Tensor: The latent space representation.
+        """
+
+        latent = self.encoder(x)
+        reconstructed = self.decoder(latent)
+        return reconstructed, latent
+
+
 class CategoricalEmbeddingEncoder:
     def __init__(self, categorical_cols, embed_dim):
         self.categorical_cols = categorical_cols
@@ -34,7 +93,9 @@ class CategoricalEmbeddingEncoder:
     def get_embedding_layer(self):
         if not self.fitted:
             raise ValueError("Call fit() before get_embedding_layer().")
-        return CategoricalEmbeddingModule(self.cardinalities, self.embedding_dim)
+        return CategoricalEmbeddingModule(
+            self.cardinalities, self.embedding_dim
+        )
 
 
 class CategoricalEmbeddingModule(nn.Module):
